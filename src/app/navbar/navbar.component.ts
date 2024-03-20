1fb4bb0adeb2e9135ca361bb790abd8ffd9a1d31
b1winner <43081501+b1winner@users.noreply.github.com>
2024-03-20 17:45:30 -0400

    First

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+}
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [CommonModule],
+  exports: [NavbarComponent]
+})
+export class NavbarModule { }
+
+/*
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+}*/
+/*
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
+*/

f147af9d453cbf1ccbf050c222081e0381755ae9
b1winner <43081501+b1winner@users.noreply.github.com>
2024-04-01 18:36:38 -0400

    Added Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TowerOfHanoiComponent } from './tower-of-hanoi/tower-of-hanoi.component';
 import {SlidingGameComponent} from "./sliding-game/sliding-game.component";
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
   { path: 'tower-of-hanoi', component: TowerOfHanoiComponent },
   {path: 'sliding-game', component: SlidingGameComponent},
-  // other routes...
+  {path: '', component: HomeComponent},
+
 ];
